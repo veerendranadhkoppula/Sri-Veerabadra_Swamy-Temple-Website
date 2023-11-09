91690f758d1b566147355aab174030a941fcce5c
veerendranadh koppula <96357491+veerendranadhkoppula@users.noreply.github.com>
2023-11-09 11:46:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+      strings: ["Badrakali Sametha", "Veerabadraya namaha"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+    });
+  });
